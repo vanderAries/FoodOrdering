00d796449932e4efec462c1b089a822cd256f35c
vanderAries <gp.bak96@gmail.com>
2024-08-01 08:15:05 +0200

    Build size selector

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,12 +1,89 @@
-import { View, Text } from "react-native";
-import { Stack, useLocalSearchParams } from "expo-router";
+import { useState } from 'react';
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import products from '@assets/data/products';
+import Button from '@/components/Button';
+
+const sizes = ['S', 'M', 'L', 'XL'];
 
 export default function ProductDetailScreen() {
   const { id } = useLocalSearchParams();
+  const [selectedSize, setSelectedSize] = useState('M');
+
+  const product = products.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {
+    console.warn('Adding to cart, size: ', selectedSize);
+  };
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
   return (
-    <View>
-      <Stack.Screen options={{ title: `Details: ${id}` }} />
-      <Text>Product Detail for id: {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image source={{ uri: product?.image }} style={styles.image} />
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+            onPress={() => setSelectedSize(size)}
+            style={[
+              styles.size,
+              {
+                backgroundColor: size === selectedSize ? 'gainsboro' : 'white',
+              },
+            ]}
+            key={size}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                { color: size === selectedSize ? 'black' : 'grey' },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+      <Text style={styles.price}>Price: ${product.price}</Text>
+      <Button onPress={addToCart} text="Add to cart" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 'auto',
+  },
+  sizes: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: 'gainsboro',
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+});
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,18 +1,20 @@
-import { Pressable, Text, Image, StyleSheet } from "react-native";
-import { Link } from "expo-router";
-import Colors from "@/constants/Colors";
-import { Product } from "@/types";
+import { Pressable, Text, Image, StyleSheet } from 'react-native';
+import { Href, Link } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { Product } from '@/types';
 
 type ProductListItemProps = {
   product: Product;
 };
 
 const defaultImage =
-  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type productDetailsLink = Href<`/menu/${number}`>;
 
 export default function ProductListItem({ product }: ProductListItemProps) {
   return (
-    <Link href={`/menu/${product.id}`} asChild>
+    <Link href={`/menu/${product.id}` as productDetailsLink} asChild>
       <Pressable style={styles.container}>
         <Image
           source={{ uri: product.image ?? defaultImage }}
@@ -28,22 +30,22 @@ export default function ProductListItem({ product }: ProductListItemProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
-    maxWidth: "50%",
+    maxWidth: '50%',
   },
   image: {
-    width: "100%",
+    width: '100%',
     aspectRatio: 1,
   },
   title: {
     fontSize: 18,
-    fontWeight: "600",
+    fontWeight: '600',
     marginVertical: 10,
   },
   price: {
     color: Colors.light.tint,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
 });
